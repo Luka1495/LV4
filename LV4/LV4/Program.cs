567b1d5b82a618f237560408301f644af9c728a7
Luka1495 <62608325+Luka1495@users.noreply.github.com>
2020-04-30 18:00:54 +0200

    Add files via upload

---
+++ LV4/LV4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dataset data = new Dataset("Luka Vukadin.csv");
+            Analyzer3rdParty analyze = new Analyzer3rdParty();
+            Adapter adapter = new Adapter(analyze);
+            Console.WriteLine(adapter.CalculateAveragePerColumn(data));
+
+            double[] res = adapter.CalculateAveragePerColumn(data);
+
+            foreach (double r in res)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
